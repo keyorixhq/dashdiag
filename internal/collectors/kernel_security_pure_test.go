50afc48eabb9784721347ed87511ca9858bd0ab4
Andrei Beshkov <andrey.beshkov@gmail.com>
2026-07-19 13:00:01 +0200

    fix: drop unneeded tc := tc copies (forvar lint, Go 1.22+ per-iter vars)
    Signed-off-by: Andrei Beshkov <andrey.beshkov@gmail.com>

--- internal/collectors/kernel_security_pure_test.go
+++ internal/collectors/kernel_security_pure_test.go
@@ -69,7 +69,6 @@ func TestExtractAVCProcesses(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			got := ExtractAVCProcesses(tc.in)
@@ -110,7 +109,6 @@ func TestIsSafePolicyToken(t *testing.T) {
 	}
 
 	for _, tc := range cases {
-		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 			got := isSafePolicyToken(tc.in)
